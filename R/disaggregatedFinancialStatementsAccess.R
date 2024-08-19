090ec666eee1591e1e77fb310f522fe84ef1ea5a
LASPUMSS <umssluisalbertosolisp@gmail.com>
2024-08-18 20:06:28 -0600

    UPDATE

---
+++ R/disaggregatedFinancialStatementsAccess.R
@@ -0,0 +1,13 @@
+#' Disaggregated Financial Statements Access
+#'
+#' Look up airline names from their carrier codes.
+#'
+#' @source <https://www.asfi.gob.bo/>
+#' @format Data frame with columns
+#' \describe{
+#' \item{CD_100_00}{Acess}
+#' \item{CD_100_XX}{Sub Access}
+#' }
+#' @examples
+#'   disaggregatedFinancialStatementsAccess
+"disaggregatedFinancialStatementsAccess"
