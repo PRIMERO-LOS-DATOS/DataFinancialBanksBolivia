611556a785312d8927f682ab454b570d813feca2
LASPUMSS <umssluisalbertosolisp@gmail.com>
2024-08-22 13:00:18 -0600

    UPDATE

--- data-raw/disaggregatedFinancialStatements_100_Assets.R
+++ data-raw/disaggregatedFinancialStatements_100_Assets.R
@@ -1,3 +1,13 @@
 ## code to prepare `disaggregatedFinancialStatements_100_Assets` dataset goes here
 
+library(gsheet)
+
+id <- '1otVD0De7frywOTaiYuAaH2M-u0jdGCN-G32rioWElLg'
+
+disaggregatedFinancialStatements_100_Assets <-
+  gsheet2tbl( paste0('docs.google.com/spreadsheets/d/', id) )
+
+save(disaggregatedFinancialStatements_100_Assets,
+     file = 'data/disaggregatedFinancialStatements_100_Assets.rda')
+
 usethis::use_data(disaggregatedFinancialStatements_100_Assets, overwrite = TRUE)
